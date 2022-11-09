fbd860f5e0feb622d6e56dcbf803f65880172e55
Kealfeyne <kealfeyneworkshop@gmail.com>
2022-11-09 21:23:09 +0300

    Edit entity for digest

--- app/entities/digest.py
+++ app/entities/digest.py
@@ -1,15 +1,30 @@
 class Digest:
     class News:
-        def __init__(self, title=None, content=None, date=None, url=None):
+        def __init__(self, title: str = None, content: str = None, date: str = None, url: str = None):
             self.title = title
             self.content = content
             self.date = date
             self.url = url
 
-    def get_data(self):
-        news = self.News()
+            return
 
-        self.news_list.append(news)
+    def __init__(self):
+        self.data = []
+
         return
 
-    news_list = []
+    def get_data(self) -> None:
+
+        try:
+            ### Связь с бэком
+            for i in range(0, 5):
+                news = self.News(title=f"Название статьи {i + 1}",
+                                 content=f"Содержание статьи {i + 1}",
+                                 date=f"0{i + 3}.11.22",
+                                 url=f"badass:{i * 111}")
+
+                self.data.append(news)
+        except Exception as err:  # Поймать нужную ошибку при отсутствии связи с бэком
+            print(err)
+
+        return
