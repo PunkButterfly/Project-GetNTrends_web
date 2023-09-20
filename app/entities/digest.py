075ade3078b42cc7f35e317d20fa7df311bff854
RomanIsaev <mr.romka.01@mail.ru>
2023-09-20 13:52:30 +0300

    Изменена логика формирования дайджеста (по категориям)

--- app/entities/digest.py
+++ app/entities/digest.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class Digest:
     class News:
         def __init__(self, title: str = None, content: str = None, date: str = None, channel: str = None):
@@ -8,9 +10,36 @@ class Digest:
 
             return
 
+    class News_by_category:
+        def __init__(self, category: str = None, content: list[str] = None, channels: list[str] = None):
+            self.category = category
+            self.content = content
+            self.channels = channels
+
+            return
+
     def __init__(self, response):
         self.data = []
-        self.parse_data(response)
+        self.all_categories = []
+        self.not_empty_cat_info = []
+        self.perse_data_categories(response)
+        #self.parse_data(response)
+
+        return
+
+    def perse_data_categories(self, response):
+        try:
+            for category, category_info in response["digest"].items():
+                news_per_cat = self.News_by_category(category=category,
+                                                     content=category_info['text'],
+                                                     channels=category_info['channel_id'])
+                if len(category_info['text']):
+                    self.not_empty_cat_info.append(category)
+                self.all_categories.append(category)
+                self.data.append(news_per_cat)
+
+        except Exception as err:
+            print(err)
 
         return
 
