a8b721543f29de2a7c4cae637955681858c7e768
RomanIsaev <mr.romka.01@mail.ru>
2023-09-20 13:54:21 +0300

    Изменен фронт дайджеста

--- app/main_page.py
+++ app/main_page.py
@@ -5,7 +5,8 @@ from error_page import error_page
 
 def main_page(received_responses):
     #periods_descriptions = [item.description for item in Period]
-
+    # received_responses: ResponseData.digest.data: List[News_by_category: .category, .content, .channels;
+    # ResponseData.insight.data: List[Insight.content]
     # Содержание страницы
     st.title("Поиск трендов, инсайтов и сборка дайджеста в источниках новостей")
 
@@ -16,17 +17,30 @@ def main_page(received_responses):
         #tabs = list(st.tabs('default'))
 
         for response in received_responses:
+            print('response proceed', response)
 
             digest = response.digest
-
-            print(digest.data[0].title, digest.data[0].content)
+            tabs = list(st.tabs(digest.all_categories))
+            print('xuy')
+            
             try:
-                for news_data in digest.data:
-                    # with st.expander(news_data.title):
-                    st.write(news_data.content)
-                            # st.caption(news_data.channel)
+                for tab, digest_data in zip(tabs, digest.data):
+                    with tab:
+                        for news_content, news_channel in zip(digest_data.content, digest_data.channels):
+                            st.write(news_content)
+                            st.caption(f'Источник: {news_channel}')
+
             except Exception:
                 error_page("Digest being prepared")
+
+
+            #try:
+                #for news_data in digest.data:
+                    # with st.expander(news_data.title):
+                    #st.write(news_data.content)
+                            # st.caption(news_data.channel)
+            #except Exception:
+                #error_page("Digest being prepared")
     '''
     with st.container():
         st.header("Текущие тренды")
