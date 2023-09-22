ed638c63c722e3ecd8a3beac2e7d6a17b4c32e46
kealfeyne <kealfeyneworkshop@gmail.com>
2023-09-22 17:02:37 +0300

    Delete unnecessary

--- app/app.py
+++ app/app.py
@@ -1,25 +1,12 @@
-from entities import Period, ResponseData
+from entities import ResponseData
 from main_page import main_page
 from error_page import error_page
 
 try:
 
-    # daily_response = ResponseData(Period.DAY)
-    # daily_response.get_response_by_period(period="day")
-    # weekly_response = ResponseData(Period.WEEK)
-    # weekly_response.get_response_by_period(period="week")
-    # monthly_response = ResponseData(Period.MONTH)
-    # monthly_response.get_response_by_period(period="month")
-    # yearly_response = ResponseData(Period.YEAR)
-    # yearly_response.get_response_by_period(period="year")
-
-    # Получение ответа вида {"insights": [],
-    # "digest": {category: {'text': [], 'channel_id': []} for category in all_cat}}
-    # И его парсинг в атрибуты digest и insight
     default_response = ResponseData('default')
     default_response.get_response_default()
-    # получаем response вида:
-    # ResponseData.digest.data: List[News_by_category: category, content, channels]
+
     received_responses = [default_response]
     main_page(received_responses)
 except Exception:
--- app/main_page.py
+++ app/main_page.py
@@ -28,46 +28,7 @@ def main_page(received_responses):
 
             except Exception:
                 error_page("Insights being prepared")
-            # tabs = list(st.tabs(digest.all_categories))
 
-            # try:
-            #     for tab, digest_data in zip(tabs, digest.data):
-            #         with tab:
-            #             for news_content, news_channel in zip(digest_data.content, digest_data.channels):
-            #                 st.write(news_content)
-            #                 st.caption(f'Источник: {news_channel}')
-            #
-            # except Exception:
-            #     error_page("Digest being prepared")
-
-
-            #try:
-                #for news_data in digest.data:
-                    # with st.expander(news_data.title):
-                    #st.write(news_data.content)
-                            # st.caption(news_data.channel)
-            #except Exception:
-                #error_page("Digest being prepared")
-    '''
-    with st.container():
-        st.header("Текущие тренды")
-
-        tabs = list(st.tabs(periods_descriptions))
-
-        for trends_tab, response in zip(tabs, received_responses):
-            with trends_tab:
-
-                trends = response.trends
-                try:
-                    for trend_data in trends.data:
-                        with st.expander(trend_data.keywords):
-                            news_by_trend = trend_data.contents
-                            for news in news_by_trend:
-                                st.write("")
-                                st.write(news)
-                except Exception:
-                    error_page("Trends being prepared")
-    '''
     with st.container():
         st.header("Инсайты")
 
